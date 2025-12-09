59eb6e46e2ed12170219786b929b0fd088c8ec75
Bethlehem <aberabetty3@gmail.com>
2025-12-09 12:27:16 -0500

    changed the variable name from country to name

--- src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class HomeController {
     @GetMapping("/")
     public String homepage(@RequestParam(required = false) String name) {
