bf81d2d1e0aa6d02764dd64b93a38adf68c0f168
Bethlehem <aberabetty3@gmail.com>
2025-12-09 12:23:15 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -0,0 +1,18 @@
+package com.pluralsight.NorthwindTradersAPI;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+    @GetMapping("/")
+    public String homepage(@RequestParam(required = false) String name) {
+        if (name != null && !name.isEmpty()) {
+            return "Hello " + name;
+        }
+        return "Hello World";
+    }
+}
+
